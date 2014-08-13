9f82ccab177fee29adf34f415be61e89ae7eb279
LMN <lmn93beo@yahoo.com>
2014-08-13 11:36:43 -0700

    There are now 2 DAQ sessions - one for continuous background recording and one for output. Added code for juice reward (set the port output to 1 if juice is given)

--- plotData.m
+++ plotData.m
@@ -4,8 +4,8 @@ global all_scans all_TimeStamps DAQstruct
 all_scans = [all_scans;event.Data];
 all_TimeStamps = [all_TimeStamps;event.TimeStamps];
 
-exceeded_threshold = event.Data(1,:)>2;
-DAQstruct.exceeded_threshold = exceeded_threshold;
+LickedList = event.Data(1,:)>2;
+DAQstruct.LickedList = LickedList;
 
 fprintf('%s: Acquired %d scans...\n',datestr(now),size(event.TimeStamps,1));
 plot(all_TimeStamps,all_scans)
