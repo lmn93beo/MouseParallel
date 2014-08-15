661f4539a58cf7f61d0335663c219d586b883826
LMN <lmn93beo@yahoo.com>
2014-08-14 17:07:47 -0700

    Fixed bug: new state should not depend only on OutputDecisionList, but also on existing state. Added the MainStruct, with currentportstate list to help keep track of ports

--- plotData.m
+++ plotData.m
@@ -1,5 +1,5 @@
 function plotData(src,event)
-global all_scans all_TimeStamps DAQstruct
+global all_scans all_TimeStamps DAQstruct MainStruct
 
 all_scans = [all_scans;event.Data];
 all_TimeStamps = [all_TimeStamps;event.TimeStamps];
@@ -7,7 +7,7 @@ all_TimeStamps = [all_TimeStamps;event.TimeStamps];
 LickedList = event.Data(1,1:4)>2;
 DAQstruct.LickedList = LickedList;
 
-fprintf('%s: Acquired %d scans...\n',datestr(now),size(event.TimeStamps,1));
+fprintf('%f: Acquired %d scans...\n',GetSecs()-MainStruct.InitTime,size(event.TimeStamps,1));%datestr(now)
 plot(all_TimeStamps,all_scans);
 leg = legend('ai0','ai1','ai2','ai3');
 
